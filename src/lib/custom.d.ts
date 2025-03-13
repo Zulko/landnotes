c7a49e2f8a787d2007fa6a984a18e5d6f7dc44f3
Zulko <valentin.zulkower@gmail.com>
2025-03-12 20:50:34 -0400

    removed unused file

--- src/lib/custom.d.ts
+++
@@ -1,9 +0,0 @@
-interface Window {
-  geodataWorker: Worker;
-  geodataWorkerPromises: {
-    [url: string]: {
-      resolve: (value: any) => void;
-      reject: (reason?: any) => void;
-    }
-  };
-} 
