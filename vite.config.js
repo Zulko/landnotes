e9caffd8db6f365a0e85d3169b599875ca980b4f
Zulko <valentin.zulkower@gmail.com>
2025-04-06 19:52:11 -0400

    cleaning stuff up

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: process.env.NODE_ENV === "production" ? "/world-of-wikipedia/" : "/",
   server: {
     proxy:
       process.env.NODE_ENV === "development"
